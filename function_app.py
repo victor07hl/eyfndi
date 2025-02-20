ebcec1009392896da3dfa9b0d46466170b6e389e
Victor Moreno <victormoreno@Victors-Laptop.local>
2025-02-20 13:38:38 -0500

    adding function_app.py

--- function_app.py
+++ function_app.py
@@ -26,7 +26,6 @@ def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
              status_code=200
         )
     
-@app.function_name(name="mycustomfunction")
 @app.route(route="mycustom")    
 def mycustom(req: func) -> func.HttpResponse:
     return func.HttpResponde("Custom custom",status_code=200)
